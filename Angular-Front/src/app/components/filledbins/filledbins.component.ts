8ca78670138687bb03a3dd0150e77b1839da7ebe
Unknown <lahirusabe15@gmail.com>
2018-03-17 07:33:23 +0530

    Driver, Bin Modals added

--- Angular-Front/src/app/components/filledbins/filledbins.component.ts
+++ Angular-Front/src/app/components/filledbins/filledbins.component.ts
@@ -9,10 +9,20 @@ import { AppComponent } from '../../app.component';
 })
 export class FilledbinsComponent implements OnInit {
   public bin_obj:any;
+  display='none';
 
   constructor(app:AppComponent) {
     //This component get from the AppComponent
     this.bin_obj = app.bins;
+  }
+
+  //After Clicking Add Bin Button this Model will be called
+  openmyModal(){
+    this.display="block";
+  }
+  //Close button on Modal
+  onCloseHandled(){
+    this.display='none';
   }
 
   ngOnInit() {
