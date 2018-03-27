9fc5f6189da87e55ef8385e6682a69c450266a1d
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2018-03-27 19:21:05 +0530

    Add new bin functionality Added

--- Angular-Front/src/app/components/filledbins/filledbins.component.ts
+++ Angular-Front/src/app/components/filledbins/filledbins.component.ts
@@ -11,6 +11,13 @@ export class FilledbinsComponent implements OnInit {
   public bin_obj:any;
   display='none';
 
+  bin = {
+    bin_id: '',
+    description: '',
+    location: ''
+  };
+
+
   constructor(app:AppComponent) {
     //This component get from the AppComponent
     this.bin_obj = app.bins;
@@ -23,6 +30,12 @@ export class FilledbinsComponent implements OnInit {
   //Close button on Modal
   onCloseHandled(){
     this.display='none';
+  }
+
+  //Add New Bin
+  add_new_bin(){
+    this.bin_obj.push(this.bin);
+    this.onCloseHandled();
   }
 
   ngOnInit() {
