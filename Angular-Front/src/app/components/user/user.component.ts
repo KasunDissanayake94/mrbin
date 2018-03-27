1f0cd4daa3f839d4ed3d7b9c7ba908697d7ac766
Unknown <lahirusabe15@gmail.com>
2018-03-27 22:10:59 +0530

    User component
    Maneesha added

--- Angular-Front/src/app/components/user/user.component.ts
+++ Angular-Front/src/app/components/user/user.component.ts
@@ -6,9 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
+  display='none';
   constructor() { }
-
+  //After Clicking Add Bin Button this Model will be called
+  openmyModal(){
+    this.display="block";
+  }
+  //Close button on Modal
+  onCloseHandled(){
+    this.display='none';
+  }
   ngOnInit() {
   }
 
