92f74793b8364ac24544d633c21aa66956bbac78
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2018-04-14 08:22:16 +0530

    Google map description added

--- Angular-Front/src/app/components/google-map/google-map.component.ts
+++ Angular-Front/src/app/components/google-map/google-map.component.ts
@@ -16,7 +16,7 @@ import { GeoService } from '../../service/geo.service'
 
       <agm-map [latitude]="lat" [longitude]="lng" [zoom]="13">
 
-        <agm-marker [latitude]="lat" [longitude]="lng">
+        <agm-marker [latitude]="lat" [longitude]="lng" >
 
           <agm-info-window>
             <h3><strong>Location</strong></h3>
@@ -32,7 +32,7 @@ import { GeoService } from '../../service/geo.service'
           <agm-info-window>
             <h3><strong>Bin Filled</strong></h3>
 
-            <p>You are {{ marker.distance }} kilometers from this point</p>
+            <p>{{ marker[3]}} </p>
           </agm-info-window>
 
         </agm-marker>
@@ -48,11 +48,11 @@ export class GoogleMapComponent implements OnInit {
   lng: number;
 //latitude and logitude
   public markers =  [
-    [6.916905, 79.867740],
-    [6.935570, 79.847956],
-    [6.927391, 79.844995],
-    [6.923727, 79.882975],
-    [6.871723, 79.879094]
+    [6.916905, 79.867740,"bin_1","Royal College Primary Section"],
+    [6.935570, 79.847956,"bin_2","Near Public Library"],
+    [6.927391, 79.844995,"bin_3","Ananda College Primary Section"],
+    [6.923727, 79.882975,"bin_4","University of Colombo faculty of Science"],
+    [6.871723, 79.879094,"bin_5","House of Fashion Bin"]
   ];
   subscription: any;
   private geo: GeoService;
