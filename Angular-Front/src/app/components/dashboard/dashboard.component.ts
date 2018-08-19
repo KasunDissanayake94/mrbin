637990264108ca11eebcfdba4e9c956975051636
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2018-08-19 14:43:15 +0530

    Maps Added to the dashboard and some changes

--- Angular-Front/src/app/components/dashboard/dashboard.component.ts
+++ Angular-Front/src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit ,OnDestroy } from '@angular/core';
+import { AppComponent } from '../../app.component';
+import { GeoService } from '../../service/geo.service'
+import { Headers , Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  private lng: number;
+  private lat: number;
 
-  constructor() { }
+  constructor() {
+    this.getUserLocation();
+  }
 
   lastCollected:number;
   todayCollect : number;
@@ -30,15 +37,23 @@ export class DashboardComponent implements OnInit {
 
     this.lastCollected = this.lastCollected+this.todayCollect;
     this.todayCollect =0;
-
-    
-
   }
+
 
   collectIt(amount : number){
     this.todayCollect = amount;
-    
 
+
+  }
+  private getUserLocation() {
+    /// locate the user
+    console.log("Google map called");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+      });
+    }
   }
 
 }
